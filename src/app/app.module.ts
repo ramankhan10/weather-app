60e40baf78e83e3accde750b8171ec9eaf658303
Raman <khanahmadi.raman@gmail.com>
2023-10-03 15:59:07 +0330

    add API and create weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WeatherComponent],
   imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Weather } from './weather';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  
-  constructor(private http :HttpClient) {}
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/';
+  private apiKey = '<6cce7dfeca7d80cc917de463f9d74805>';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<Weather> {
+    const options = new HttpParams()
+      .set('units', 'metric')
+      .set('q', city)
+      .set('appId', this.apiKey);
+
+    return this.http.get<Weather>(this.apiUrl + 'weather', {
+      params: options,
+    });
+  }
 }
